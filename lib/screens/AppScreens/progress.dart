80a335eb52be3ecf78d5dfa1f886dabddd7c9b74
unknown <sinchu1729@gmail.com>
2021-04-03 10:22:20 +0530

    sidebar

--- lib/screens/AppScreens/progress.dart
+++ lib/screens/AppScreens/progress.dart
@@ -8,41 +8,45 @@ class progress extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final today = TimeUtils.removeTime(DateTime.now());
-    return Column(
-      children: [
-        Text(
-          'Heatmap calendar',
-          style: Theme.of(context).textTheme.headline4,
+    return Scaffold(
+        body: SafeArea(
+          child: HeatMapCalendar(
+            input: {
+              TimeUtils.removeTime(DateTime.now().subtract(Duration(days: 3))):
+                  5,
+              TimeUtils.removeTime(DateTime.now().subtract(Duration(days: 2))):
+                  35,
+              TimeUtils.removeTime(DateTime.now().subtract(Duration(days: 1))):
+                  14,
+              TimeUtils.removeTime(DateTime.now()): 5,
+            },
+            colorThresholds: {
+              1: Colors.green[100],
+              10: Colors.green[300],
+              30: Colors.green[500]
+            },
+            weekDaysLabels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+            monthsLabels: [
+              "",
+              "Jan",
+              "Feb",
+              "Mar",
+              "Apr",
+              "May",
+              "Jun",
+              "Jul",
+              "Aug",
+              "Sep",
+              "Oct",
+              "Nov",
+              "Dec",
+            ],
+            squareSize: 20.0,
+            textOpacity: 0.3,
+            labelTextColor: Colors.blueGrey,
+            dayTextColor: Colors.blue[500],
+          ),
         ),
-        HeatMapCalendar(
-          // !! Prepare the data: mapping from dates and integers
-          input: {
-            today.subtract(const Duration(days: 1)): 40,
-            today.subtract(const Duration(days: 2)): 30,
-            today.subtract(const Duration(days: 3)): 10,
-            today.subtract(const Duration(days: 5)): 30,
-            today.subtract(const Duration(days: 7)): 15,
-            today.subtract(const Duration(days: 10)): 5,
-            today.subtract(const Duration(days: 14)): 10,
-            today.subtract(const Duration(days: 28)): 5,
-          },
-          // ! Set color map.
-          colorThresholds: {
-            1: Colors.green[100],
-            10: Colors.green[300],
-            30: Colors.green[500],
-          },
-          squareSize: 18,
-          textOpacity: 0.3,
-          labelTextColor: Colors.blueGrey,
-          dayTextColor: Colors.blue[500],
-          // Week day and month labels can be overriden:
-          // weekDaysLabels: const ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-          // monthsLabels: const ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-          //    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        ),
-        const Text('(Double tap to toggle dates text)'),
-      ],
-    );
+      ),
   }
 }
