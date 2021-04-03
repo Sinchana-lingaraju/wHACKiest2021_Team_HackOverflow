106f9e323ec8adf67847edd1740ad7ab851191c2
16o1kym <16o1kym@gmail.com>
2021-04-03 22:18:42 +0530

    Logo changed

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:whackiest/services/authService.dart';
 import 'package:whackiest/wrapper.dart';
+import 'package:flutter_launcher_icons/android.dart';
+import 'package:flutter_launcher_icons/constants.dart';
+import 'package:flutter_launcher_icons/custom_exceptions.dart';
+import 'package:flutter_launcher_icons/ios.dart';
+import 'package:flutter_launcher_icons/main.dart';
+import 'package:flutter_launcher_icons/utils.dart';
+import 'package:flutter_launcher_icons/xml_templates.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
---
+++ lib/screens/AuthScreens/register.dart
@@ -0,0 +1,132 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:whackiest/screens/AppScreens/homepage.dart';
+import 'package:whackiest/services/authService.dart';
+
+import 'login.dart';
+
+class Register extends StatefulWidget {
+  Register({Key key}) : super(key: key);
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+//
+  @override
+//
+//
+  AuthService _auth = AuthService();
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  String email;
+  Widget _buildemail() {
+    return TextFormField(
+      onChanged: (String val) {
+        email = val;
+      },
+      decoration: InputDecoration(labelText: "Email"),
+      validator: (String val) {
+        if (val.isEmpty) {
+          return "Email is required";
+        }
+        return null;
+      },
+    );
+  }
+
+  String password;
+  Widget _buildpassword() {
+    return TextFormField(
+      onSaved: (String val) {
+        password = val;
+      },
+      decoration: InputDecoration(labelText: "Password"),
+      validator: (String val) {
+        if (val.isEmpty) {
+          return "Password is required.";
+        } else if (val.length < 6) {
+          return "Password should be atleast 6 characters long.";
+        }
+        return null;
+      },
+    );
+  }
+
+//
+//
+//......using bool visibilty to show a text that veification email has been sent after clicking submit ...
+//
+//
+  bool visibility = false;
+//
+//
+//......using bool verified to check if email is verified or not.
+//
+//
+  bool verified;
+  Timer timer;
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Register"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+          child: Form(
+            key: _formkey,
+            child: ListView(
+              children: [
+                _buildemail(),
+                _buildpassword(),
+                SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      if (!_formkey.currentState.validate()) return;
+                      _formkey.currentState.save();
+                      // call method to signup
+                      User user = await _auth.createUserWithEmailAndPAss(
+                          email: email, password: password);
+                      if (user != null) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Homepage()));
+                      }
+                    },
+                    child: Text("Submit")),
+                SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    await _auth.googleSignin().then((value) {
+                      if (value.emailVerified)
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (_) => Homepage()));
+                    });
+                  },
+                  child: Text("Sign up using google"),
+                ),
+                SizedBox(
+                  height: 150,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) => Login()));
+                    },
+                    child: Text("login screen")),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
