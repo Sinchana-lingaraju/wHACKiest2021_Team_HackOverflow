20d5315792d71a32912d2de706062cea992d8472
unknown <sinchu1729@gmail.com>
2021-04-03 10:53:46 +0530

    audio

--- lib/screens/AppScreens/log.dart
+++ lib/screens/AppScreens/log.dart
@@ -1,10 +1,65 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
 
-class Log extends StatelessWidget {
-  const Log({Key key}) : super(key: key);
+class Log extends StatefulWidget {
+  Log({Key key}) : super(key: key);
 
   @override
+  _LogState createState() => _LogState();
+}
+
+class _LogState extends State<Log> {
+  int _currentMood;
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text("sleep"));
+    return GetMaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              child: Text(
+                  "Hello " + FirebaseAuth.instance.currentUser.displayName),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add_box),
+          onPressed: () {
+            Get.bottomSheet(SingleChildScrollView(
+              child: Container(
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Text("Title: "),
+                    TextField(),
+                    Text("Content: "),
+                    TextField(),
+                    Text("How's you feelin!!"),
+                    SfSlider(
+                      min: 0.0,
+                      max: 100.0,
+                      value: 100.0,
+                      interval: 20,
+                      showTicks: true,
+                      showLabels: true,
+                      enableTooltip: true,
+                      minorTicksPerInterval: 1,
+                      onChanged: (dynamic value) {
+                        setState(() {
+                          _currentMood = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ));
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/screens/AppScreens/sleep.dart
+++ lib/screens/AppScreens/sleep.dart
@@ -1,10 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:wave/wave.dart';
+import 'package:wave/config.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
-class Sleep extends StatelessWidget {
-  const Sleep({Key key}) : super(key: key);
+// import 'package:audio_service/audio_service.dart';
+
+class Sleep extends StatefulWidget {
+  Sleep({Key key}) : super(key: key);
+
+  @override
+  _SleepState createState() => _SleepState();
+}
+
+class _SleepState extends State<Sleep> {
+  @override
+  // void initState() async {
+  //   super.initState();
+  //   ListResult listResult = await storage.ref().child('').list();
+  //   setState(() {
+  //     references = listResult.items;
+  //     print(references);
+  //   });
+  // }
+
+  AudioPlayer audioPlayer = AudioPlayer();
+
+  playAudio(String url) {
+    audioPlayer.play(url);
+  }
+
+  pauseAudio() {
+    audioPlayer.pause();
+  }
+
+  stopAudio() {
+    audioPlayer.stop();
+  }
+
+  List<Reference> references;
+  var storage = FirebaseStorage.instance;
+
+  Widget tile(String title) {
+    return ListTile(
+      leading: CircleAvatar(),
+      title: Text(title),
+      subtitle: Text(
+          'Connect your headphones and set the volume to a comfortable level.'),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("sleep"));
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(height: 40),
+          GestureDetector(
+            child: tile("Relaxation, Stress Reduction"),
+            onTap: () async {
+              String url =
+                  await storage.ref().child('Alpha.mp3').getDownloadURL();
+              print(url);
+              playAudio(url);
+            },
+          ),
+          GestureDetector(
+            child: tile("Awareness, Alertness"),
+            onTap: () async {
+              String url =
+                  await storage.ref().child('Beta.mp3').getDownloadURL();
+              print(url);
+              playAudio(url);
+            },
+          ),
+          GestureDetector(
+            child: tile("REM Sleep. Power Naps"),
+            onTap: () async {
+              String url =
+                  await storage.ref().child('Theta.mp3').getDownloadURL();
+              print(url);
+              playAudio(url);
+            },
+          ),
+          GestureDetector(
+            child: tile("Focus, Concentration, Information Processing"),
+            onTap: () async {
+              String url =
+                  await storage.ref().child('Gamma.mp3').getDownloadURL();
+              print(url);
+              playAudio(url);
+            },
+          ),
+          GestureDetector(
+            child: tile("Deep Sleep. Recovery"),
+            onTap: () async {
+              String url =
+                  await storage.ref().child('Delta.mp3').getDownloadURL();
+              print(url);
+              playAudio(url);
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
